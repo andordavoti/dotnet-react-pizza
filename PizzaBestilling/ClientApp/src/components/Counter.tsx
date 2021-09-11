ac202d47575927439b0cec4b1ed6ecdd1a4d771d
andordavoti <andor.davoti@gmail.com>
2021-09-11 13:25:45 +0200

    refactored react to use functional components with hooks

---
+++ PizzaBestilling/ClientApp/src/components/Counter.tsx
@@ -0,0 +1,25 @@
+﻿import { FC, useState } from "react";
+
+const Counter: FC = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter</h1>
+
+      <p>This is a simple example of a React component.</p>
+
+      <p aria-live="polite">
+        Current count: <strong>{count}</strong>
+      </p>
+
+      <button
+        className="btn btn-primary"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ PizzaBestilling/ClientApp/src/components/Layout.tsx
@@ -0,0 +1,14 @@
+﻿import { FC } from "react";
+import { Container } from "reactstrap";
+import NavMenu from "./NavMenu";
+
+const Layout: FC = ({ children }) => {
+  return (
+    <div>
+      <NavMenu />
+      <Container>{children}</Container>
+    </div>
+  );
+};
+
+export default Layout;
